db1a160ec3302de2ca8e60cf11dc1d6e0c3f8d2b
jantsigit <jan.lohva@gmail.com>
2022-09-16 11:39:42 +0300

    lisätty updateTableware ja deleteTableware funktiot. Päivitetty readAllTableware funktio näyttämään lokaatio

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ const pool = new Pool({
 })
 
 async function readAllTableware() {
-  const res = await pool.query("SELECT * FROM tableware")
+  const res = await pool.query(
+    "select tableware.name, tableware.qty, office.location from tableware inner join office on tableware.office_id=office.id"
+  )
   console.log(res.rows)
   return res.rows
 }
@@ -25,12 +27,23 @@ async function createTableware(nimi, maara, officeID) {
   console.log(`Lisätty  ${nimi} ${maara} kpl tableware tauluun`)
 }
 
-createTableware("kattila", 3, 3)
-//timeout jotta uusin päivitys näkyy myös
-/*
-setTimeout(() => {
-  readAllTableware()
-}, 2000)*/
+async function updateTableware(id, nimi, maara, officeID) {
+  await pool.query(
+    "UPDATE tableware SET name=$2, qty=$3, office_id=$4 WHERE id = $1",
+    [id, nimi, maara, officeID]
+  )
+  console.log(`Muutettu  ID:${id} tietoja tableware taulussa`)
+}
+
+async function deleteTableware(id) {
+  await pool.query("DELETE FROM tableware WHERE id=$1", [id])
+  console.log(`Poistettu ID:${id} tableware taulusta`)
+}
+
+//readAllTableware()
+//deleteTableware(12)
+//updateTableware(1, "lautanen", 3, 2)
+//createTableware("kattila", 3, 3)
 
 async function createOffice(name, location, startYear) {
   await pool.query(
